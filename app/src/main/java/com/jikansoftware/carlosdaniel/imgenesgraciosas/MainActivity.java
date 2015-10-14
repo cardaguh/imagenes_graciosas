4e224332f52bba94378a70ac23b26620d5d3e300
cardaguh <cardaguh@gmail>
2015-10-14 09:48:10 -0500

    Aplicacion para reirse con imágenes graciosas
    Puedes ver el codigo y modificarlo y mejorarla la interfaz de usuario,
    que sea más interactiva y con interfaz con redes sociales y permita
    cargar imágenes desde otras fuentes.

---
+++ app/src/main/java/com/jikansoftware/carlosdaniel/imgenesgraciosas/MainActivity.java
@@ -0,0 +1,119 @@
+package com.jikansoftware.carlosdaniel.imgenesgraciosas;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Integer[] imageIDs = {
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+            R.drawable.img7,
+            R.drawable.img8,
+            R.drawable.img9,
+            R.drawable.img10,
+            R.drawable.img11,
+            R.drawable.img12,
+            R.drawable.img13,
+            R.drawable.img14,
+            R.drawable.img15,
+            R.drawable.img16,
+            R.drawable.img17,
+            R.drawable.img18,
+            R.drawable.img19,
+            R.drawable.img20,
+            R.drawable.img21,
+            R.drawable.img22,
+            R.drawable.img23,
+            R.drawable.img24,
+            R.drawable.img25,
+            R.drawable.img26,
+            R.drawable.img27,
+            R.drawable.img28,
+            R.drawable.img29,
+            R.drawable.img30,
+            R.drawable.img31,
+            R.drawable.img32,
+            R.drawable.img33,
+            R.drawable.img34,
+            R.drawable.img35,
+            R.drawable.img36,
+            R.drawable.img37,
+            R.drawable.img38,
+            R.drawable.img39,
+            R.drawable.img40,
+            R.drawable.img41,
+            R.drawable.img42,
+            R.drawable.img43,
+            R.drawable.img44,
+            R.drawable.img45,
+            R.drawable.img46,
+            R.drawable.img47,
+            R.drawable.img48,
+            R.drawable.img49,
+            R.drawable.img50,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Note that Gallery view is deprecated in Android 4.1---
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        gallery.setAdapter(new ImageAdapter(this));
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getBaseContext(), "Imagen " + (position + 1) + " seleccionada",
+                        Toast.LENGTH_SHORT).show();
+                // display the images selected
+                ImageView imageView = (ImageView) findViewById(R.id.image1);
+                imageView.setImageResource(imageIDs[position]);
+            }
+        });
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+        private int itemBackground;
+        public ImageAdapter(Context c)
+        {
+            context = c;
+            // sets a grey background; wraps around the images
+            TypedArray a =obtainStyledAttributes(R.styleable.MyGallery);
+            itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
+            a.recycle();
+        }
+        // returns the number of images
+        public int getCount() {
+            return imageIDs.length;
+        }
+        // returns the ID of an item
+        public Object getItem(int position) {
+            return position;
+        }
+        // returns the ID of an item
+        public long getItemId(int position) {
+            return position;
+        }
+        // returns an ImageView view
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(imageIDs[position]);
+            imageView.setLayoutParams(new Gallery.LayoutParams(100, 100));
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
+    }
+}
